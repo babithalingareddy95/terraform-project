7c6998f836653bd2856d82f0c7ca8c38b2165cd3
babithalingareddy95 <139122287+babithalingareddy95@users.noreply.github.com>
2023-08-25 13:42:09 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 #this file consists of code for instances and sg
 provider "aws" {
 region = "eu-west-3"
-access_key = ""
-secret_key = ""
+access_key = "AKIATMOGS62MVPBIPKU4"
+secret_key = "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"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-0b8b5288592eca360"
+  ami             = "ami-0041b98fa770e38cd"
   instance_type   = "t2.micro"
-  key_name        = "terrapem"
+  key_name        = "practicepem"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "eu-west-3a"
   user_data       = <<EOF
@@ -25,9 +25,9 @@ EOF
 }
 
 resource "aws_instance" "two" {
-  ami             = "ami-0b8b5288592eca360"
+  ami             = "ami-0041b98fa770e38cd"
   instance_type   = "t2.micro"
-  key_name        = "terrapem"
+  key_name        = "practicepem"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "eu-west-3b"
   user_data       = <<EOF
@@ -44,9 +44,9 @@ EOF
 }
 
 resource "aws_instance" "three" {
-  ami             = "ami-0b8b5288592eca360"
+  ami             = "ami-0041b98fa770e38cd"
   instance_type   = "t2.micro"
-  key_name        = "terrapem"
+  key_name        = "practicepem"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "eu-west-3a"
   tags = {
@@ -55,9 +55,9 @@ resource "aws_instance" "three" {
 }
 
 resource "aws_instance" "four" {
-  ami             = "ami-0b8b5288592eca360"
+  ami             = "ami-0041b98fa770e38cd"
   instance_type   = "t2.micro"
-  key_name        = "terrapem"
+  key_name        = "practicepem"
   vpc_security_group_ids = [aws_security_group.five.id]
   availability_zone = "eu-west-3b"
   tags = {
@@ -90,7 +90,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "rahamshaikterra7gsywgys7889900prodenvgshj"
+  bucket = "babithaterra7gsywgys7889900prodenvgshj"
 }
 
 resource "aws_iam_user" "seven" {
