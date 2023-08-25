15bc9d7f921d708296ce2aa058f528c29b51bea8
babithalingareddy95 <139122287+babithalingareddy95@users.noreply.github.com>
2023-08-25 17:13:56 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_security_group" "five" {
 }
 
 resource "aws_s3_bucket" "six" {
-  bucket = "rahamshaikterra7gsywgys7889900prodenvgshj"
+  bucket = "babitha251195"
 }
 
 resource "aws_iam_user" "seven" {
